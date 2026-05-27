0b81b7e2b4fb554da827a010d09f5808904fdc6d
Emanuele Mottola <emanuele.mottola94@gmail.com>
2026-05-27 22:22:10 +0200

    First commit

---
+++ src/main/kotlin/com/xazy/menify/MenifyApplication.kt
@@ -0,0 +1,11 @@
+package com.xazy.menify
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MenifyApplication
+
+fun main(args: Array<String>) {
+	runApplication<MenifyApplication>(*args)
+}
---
+++ src/test/kotlin/com/xazy/menify/MenifyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xazy.menify
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MenifyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
